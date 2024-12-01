7ca5fa0307ad5cf886df9f94a01aeb93a7f23f8e
NorahAlwohaibi <144901589+norah-mohammed@users.noreply.github.com>
2024-12-01 03:05:56 +0300

    Update registerRetailer.js

--- scripts/registerRetailer.js
+++ scripts/registerRetailer.js
@@ -5,25 +5,25 @@ module.exports = async function (callback) {
     const roleContract = await RoleContract.deployed();
     const accounts = await web3.eth.getAccounts();
 
-    // Specific details for Carrefour KSA
+    // Specific details for Danube
     const retailerAddress = "0xcE1072a87a0edAF7508CfeC4d31594E7c20b75F5"; // Ethereum address of the retailer
 
-    // Add Carrefour KSA as a retailer
+    // Add Danube as a retailer
     await roleContract.addRetailer(
       retailerAddress,                          // Ethereum address of the retailer
-      "Carrefour KSA",                          // Retailer name as Carrefour KSA
-      "King Fahd Road, Riyadh",                 // Specific physical address in Riyadh, Saudi Arabia
+      "Danube",                                 // Retailer name as Danube
+      "King Fahd Road, Riyadh",                 // Physical address in Riyadh, Saudi Arabia
       "0112345678",                             // Local phone number format
-      "info@carrefourksa.com",                  // Localized email address for Carrefour KSA
-      { from: accounts[0] }                      // Transaction sent from the first account (admin/owner)
+      "info@danube.com",                        // Email address for Danube
+      { from: accounts[0] }                     // Transaction sent from the first account (admin/owner)
     );
 
-    console.log("Carrefour KSA registered successfully!");
+    console.log("Danube registered successfully!");
 
     // Retrieve retailer info from the contract (assuming a function like getRetailer exists)
     const retailerInfo = await roleContract.getRetailer(retailerAddress);
 
-    // Print out Carrefour KSA's information
+    // Print out Danube's information
     console.log("Retailer Information:");
     console.log("Ethereum Address:", retailerInfo[0]);
     console.log("Name:", retailerInfo[1]);
@@ -33,7 +33,7 @@ module.exports = async function (callback) {
 
     callback();  // End the script
   } catch (error) {
-    console.error("Error registering Carrefour KSA:", error);
+    console.error("Error registering Danube:", error);
     callback(error);
   }
 };
